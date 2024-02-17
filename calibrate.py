12c4d825d2ec91453216a7f6b2454c4cd1e6fcc2
B00t <walibula118@gmail.com>
2024-02-17 17:04:53 +0100

    update
    finished cube & coordinate(temporarily)

--- calibrate.py
+++ calibrate.py
@@ -15,7 +15,9 @@ def calibrate(objpoints, imgpoints):
     # Prepare object points, like (0, 0, 0), (1, 0, 0), ..., (6, 5, 0)
     objp = np.zeros((6 * 9, 3), np.float32)
     objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
-    axis = np.float32([[5, 0, 0], [0, 5, 0], [0, 0, -5]]).reshape(-1, 3)
+    axis_3D = np.float32([[6, 0, 0], [0, 6, 0], [0, 0, -6]]).reshape(-1, 3)
+    axis_cube = np.float32([[0, 0, 0], [0, 3, 0], [3, 3, 0], [3, 0, 0], [0, 0, -3], [0, 3, -3], [3, 3, -3], [3, 0, -3]])
+
 
     # Calibration
     ret, mtx, dist, _, _ = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
@@ -42,11 +44,24 @@ def calibrate(objpoints, imgpoints):
         # Find the rotation and translation vectors.
         ret, rvecs, tvecs = cv.solvePnP(objp, corners2, mtx, dist)
         # project 3D points to image plane
-        imgpts, jac = cv.projectPoints(axis, rvecs, tvecs, mtx, dist)
+        # Draw the coordinate
+        imgpts_3D, jac = cv.projectPoints(axis_3D, rvecs, tvecs, mtx, dist)
         corner = tuple(np.round(corners2[0].ravel()).astype(int))
-        img = cv.line(img, corner, tuple(np.round(imgpts[0].ravel()).astype(int)), (255, 0, 0), 10)
-        img = cv.line(img, corner, tuple(np.round(imgpts[1].ravel()).astype(int)), (0, 255, 0), 10)
-        img = cv.line(img, corner, tuple(np.round(imgpts[2].ravel()).astype(int)), (0, 0, 255), 10)
+        img = cv.line(img, corner, tuple(np.round(imgpts_3D[0].ravel()).astype(int)), (255, 0, 0), 10)
+        img = cv.line(img, corner, tuple(np.round(imgpts_3D[1].ravel()).astype(int)), (0, 255, 0), 10)
+        img = cv.line(img, corner, tuple(np.round(imgpts_3D[2].ravel()).astype(int)), (0, 0, 255), 10)
+
+        # Draw the cube
+        imgpts_cube, jac = cv.projectPoints(axis_cube, rvecs, tvecs, mtx, dist)
+        imgpts_cube = np.int32(imgpts_cube).reshape(-1, 2)
+        # draw ground floor in green
+        img = cv.drawContours(img, [imgpts_cube[:4]], -1, (0, 255, 0), -3)
+        # draw pillars in blue color
+        for i, j in zip(range(4), range(4, 8)):
+            img = cv.line(img, tuple(imgpts_cube[i]), tuple(imgpts_cube[j]), (255), 3)
+        # draw top layer in red color
+        img = cv.drawContours(img, [imgpts_cube[4:]], -1, (0, 0, 255), 3)
+
         # Show the image
         # Adjust the size of window
         cv.namedWindow('painted img', cv.WINDOW_NORMAL)
