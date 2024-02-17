0de9c2b0d1a609e75809f32b0f1bba80f81f1d21
LuiHol <125266084+LuiHol@users.noreply.github.com>
2024-02-17 22:52:10 +0100

    webcam.py

--- webcam.py
+++ webcam.py
@@ -1,24 +1,45 @@
+import numpy as np
 import cv2 as cv
 
-# Create a VideoCapture object and open the Webcam
+# termination criteria
+criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
+# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+objp = np.zeros((6 * 9, 3), np.float32)
+objp[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
+
+# Arrays to store object points and image points from all the images.
+objpoints = []  # 3d point in real world space
+imgpoints = []  # 2d points in image plane.
+
+# Capture video from webcam
 cap = cv.VideoCapture(0)
-# The parameter '0' represents the default camera device.
-# If you have multiple cameras, you can try different index values (e.g., 1, 2) to select other cameras.
 
 while True:
-    # Read video frames in a loop
     ret, frame = cap.read()
     if not ret:
-        break
-    # Perform operations on each frame here
-
-    # Display the frame
-    cv.imshow('Webcam', frame)
-
-    # Check for keyboard input and exit the loop when 'q' is pressed
-    if cv.waitKey(1) == ord('q'):
+        print("Failed to capture frame from webcam")
         break
 
-# Release resources and close the window
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Find the chess board corners
+    ret, corners = cv.findChessboardCorners(gray, (9, 6), None)
+    cv.imshow('webcam', frame)
+
+    # If found, add object points, image points (after refining them)
+    if ret == True:
+        objpoints.append(objp)
+        corners2 = cv.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+        imgpoints.append(corners2)
+
+        # Draw and display the corners
+        cv.drawChessboardCorners(frame, (9, 6), corners2, ret)
+        cv.imshow('Calibration', frame)
+
+    # Wait for a key press and break the loop if 'q' is pressed
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the capture
 cap.release()
 cv.destroyAllWindows()
