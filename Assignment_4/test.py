e18e2a63e646086ee09991df813481d1851430d5
B00t <walibula118@gmail.com>
2024-03-26 21:39:16 +0100

    update

--- Assignment_4/test.py
+++ Assignment_4/test.py
@@ -1,1 +1,4 @@
 from data_process import get_data
+
+# test loader
+# test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
--- Assignment_4/train_validate.py
+++ Assignment_4/train_validate.py
@@ -1,6 +1,7 @@
 from data_process import get_data
 import models
 
+import os
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -8,6 +9,8 @@ import torch.optim as optim
 def train(model, train_loader, device, optimizer, loss_func):
     model.train()
     running_loss = 0.0
+    correct = 0
+    total = 0
     for images, labels in train_loader:
         images, labels = images.to(device), labels.to(device)
         optimizer.zero_grad()
@@ -16,29 +19,61 @@ def train(model, train_loader, device, optimizer, loss_func):
         loss.backward()
         optimizer.step()
         running_loss += loss.item() * images.size(0)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+    # calculate LOSS & ACC
     train_loss = running_loss / len(train_loader.dataset)
+    train_acc = correct / total
 
-    return model, train_loss
+    return model, train_loss, train_acc
 
-def validate(model, test_loader, device, optimizer, loss_func):
+def validate(model, val_loader, device, loss_func):
     model.eval()
+    running_loss = 0.0
     correct = 0
     total = 0
     with torch.no_grad():
-        for images, labels in test_loader:
+        for images, labels in val_loader:
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
+            loss = loss_func(outputs, labels)
+            outputs = model(images)
+            running_loss += loss.item() * images.size(0)
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
-    test_acc = correct / total
 
-    return model, test_acc
+    # calculate LOSS & ACC
+    val_loss = running_loss / len(val_loader.dataset)
+    val_acc = correct / total
+
+    return model, val_loss, val_acc
+
+def save_model(model):
+    # find the next available subdirectory name
+    save_dir = 'models'
+    sub_dirs = os.listdir(save_dir)
+    sub_dirs = [name for name in sub_dirs if os.path.isdir(os.path.join(save_dir, name))]
+    sub_dirs = sorted(sub_dirs, key=lambda x: int(x))
+    if len(sub_dirs) > 0:
+        new_dir_num = 1 + int(sub_dirs[-1])
+    else:
+        new_dir_num = 1
+
+    # create the subdirectory for saving the trained model
+    sub_dir = f'{new_dir_num:02}'
+    save_path = os.path.join(save_dir, sub_dir)
+    os.makedirs(save_path, exist_ok=True)
+    # save the trained model in the subdirectory
+    save_file = os.path.join(save_path, 'saved_model.pth')
+    torch.save(model.state_dict(), save_file)
+    print(f"Model saved in {save_file}.")
 
 def train_validate(num_epochs):
     # check if the device use GPU/CPU
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
     print(device)
     # get data
     train_dataset, val_dataset, test_dataset = get_data()
@@ -46,8 +81,8 @@ def train_validate(num_epochs):
     # get loader
     # train loader
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True)
-    # test loader
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False)
+    # validate loader
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=False)
 
     # initialize model
     # baseline
@@ -60,35 +95,14 @@ def train_validate(num_epochs):
     # train & test
 
     for epoch in range(num_epochs):
-        '''
-        model.train()
-        running_loss = 0.0
-        for images, labels in train_loader:
-            images, labels = images.to(device), labels.to(device)
-            optimizer.zero_grad()
-            outputs = model(images)
-            loss = loss_func(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item() * images.size(0)
-        train_loss = running_loss / len(train_loader.dataset)
 
-        model.eval()
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for images, labels in test_loader:
-                images, labels = images.to(device), labels.to(device)
-                outputs = model(images)
-                _, predicted = torch.max(outputs.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
-        test_acc = correct / total
-        '''
-        model, train_loss = train(model, train_loader, device, optimizer, loss_func)
-        model, test_acc = validate(model, test_loader, device, optimizer, loss_func)
+        model, train_loss, train_acc = train(model, train_loader, device, optimizer, loss_func)
+        model, val_loss, val_acc = validate(model, val_loader, device, loss_func)
 
-        print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss:.4f}, Test Accuracy: {test_acc:.4f}")
+        print(f"Epoch [{epoch+1}/{num_epochs}], Train Loss: {train_loss:.4f}, Train Accuracy: {train_acc:.4f}, Val Loss: {val_loss:.4f}, Val Accuracy: {val_acc:.4f}")
 
-num_epochs = 15
+    # Save the model file
+    save_model(model)
+
+num_epochs = 5
 train_validate(num_epochs)
