48195c9c54835946ef61284ce89707cdae5a3207
B00t <walibula118@gmail.com>
2024-03-27 17:38:16 +0100

    update

--- Assignment_4/assignment4.py
+++ Assignment_4/assignment4.py
@@ -2,7 +2,7 @@ from train_validate import train_validate
 from test import test
 
 if __name__ == "__main__":
-    num_epoch = 10
+    num_epoch = 15
 
     print("Pls input the model you want to train and test(press): baseline(0), var1(1), var2(2), var3(3), var4(4)")
     choice_str = input("Your choice:")
